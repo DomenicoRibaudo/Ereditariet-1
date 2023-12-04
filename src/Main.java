3690fd3a4c9ec929d7192bd95b282b056a44f3b5
Domenico <domenicoribaudo06@gmail.com>
2023-12-05 00:20:12 +0100

    prima commit, esercizio completo

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Animale animaleGen = new Animale();
+        Gatto gatto = new Gatto();
+        animaleGen.faiIlVerso();
+        gatto.faiIlVerso();
+
+    }
+}
